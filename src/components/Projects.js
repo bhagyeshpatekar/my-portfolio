31061295a957352c75c12088e6e684a895f629ae
Bhagyesh Patekar <86484683+bhagyeshpatekar@users.noreply.github.com>
2024-04-30 23:58:52 +0530

    Add files via upload

---
+++ src/components/Projects.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+
+// import data
+import { projectsNav } from "../data";
+
+// import components
+import Project from "./Project";
+
+const Projects = () => {
+  const [item, setItem] = useState({ name: "all" });
+  const [projects] = useState([
+    {
+      name: "ToDo App",
+      img: "https://vaibhav-darvekar-portfolio.netlify.app/static/media/todogif.fd727e07.gif",
+      feature: "Todo Application made with ReactJs. Tech-stack : [React] ",
+      Link: "https://todo-app-amber-six.vercel.app/",
+      Github: "https://github.com/bhagyeshpatekar/to-do-app",
+    },
+    {
+      name: "MI Store Clone App",
+      img: "https://github.com/vaibhav123-dev/vaibhav123-dev/assets/81352701/1ad6aac5-1c59-4120-9190-5bd96638853b",
+      feature: "MI Store Clone App made with ReactJs. Tech-stack : [React] ",
+      Link: "https://mi-store-clone-delta.vercel.app/",
+      Github: "https://github.com/bhagyeshpatekar/mi-store",
+    },
+    {
+      name: "Zomato Clone App",
+      img: "https://github.com/vaibhav123-dev/vaibhav123-dev/assets/81352701/3f8bd979-76a8-4abd-abf8-2e5e0de65771",
+      feature: "Zomato made with ReactJs. Tech-stack : [React] ",
+      Link: "https://zomato-clone-gold-pi.vercel.app/",
+      Github: "https://github.com/bhagyeshpatekar/zomato-clone",
+    },
+  ]);
+  const [active, setActive] = useState(0);
+  useEffect(() => {
+    // get projects based on item
+    if (item.name === "all") {
+      // setProjects(projectsData);
+    } else {
+      // setProjects(newProjects);
+    }
+  }, [item]);
+
+  const handleClick = (e, index) => {
+    setItem({ name: e.target.textContent.toLowerCase() });
+    setActive(index);
+  };
+
+  return (
+    <div>
+      {/* projects nav */}
+      <nav className="mb-12 max-w-xl mx-auto">
+        <ul className="flex flex-col md:flex-row justify-evenly items-center text-white">
+          {projectsNav.map((item, index) => {
+            return (
+              <li
+                onClick={(e) => {
+                  handleClick(e, index);
+                }}
+                className={`${active === index ? "active" : ""
+                  } cursor-pointer capitalize m-4`}
+                key={index}
+              >
+                {item.name}
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+      {/* projects */}
+      <section className="grid gap-y-12 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-8">
+        {projects.map((item) => {
+          return <Project item={item} key={item.id} />;
+        })}
+      </section>
+    </div>
+  );
+};
+
+export default Projects;
